da377f900c27c34de972018ef54efa6152ab317a
Patricio Uskaer <patriciouskaer@gmail.com>
2023-03-19 10:47:07 -0300

    landing page

--- speakify/src/App.js
+++ speakify/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="container">
-      <LandingPage />
+    <div>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={< LandingPage />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
