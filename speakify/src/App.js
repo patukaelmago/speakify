48ad995cab455534d5aabcd814f5858447d464ed
Patricio Uskaer <patriciouskaer@gmail.com>
2023-03-19 10:40:29 -0300

    LandingPage

--- speakify/src/App.js
+++ speakify/src/App.js
@@ -1,15 +1,14 @@
-
+import React from 'react';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Síntesis de Voz en Texto (TTS) con JavaScript</h1>
-	<p>Escribe el texto que deseas convertir en audio:</p>
-	<textarea id="texto" rows="4" cols="50"></textarea>
-	<button onclick="hablar()">Hablar</button>
+    <div className="container">
+      <LandingPage />
     </div>
   );
 }
 
 export default App;
+
---
+++ speakify/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="container">
+      <h1 className="title">Mi sitio web</h1>
+      <p className="description">¡Bienvenido a mi sitio web!</p>
+      <button className="button">Ver más</button>
+    </div>
+  );
+}
+
+export default LandingPage;
