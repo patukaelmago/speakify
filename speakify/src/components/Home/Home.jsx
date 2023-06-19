80f16315d9355d83a2366ca8a180bebfc61742a8
Patricio Uskaer <patriciouskaer@gmail.com>
2023-06-18 23:25:37 -0300

    react icons

--- speakify/src/components/Home/Home.jsx
+++ speakify/src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
+import { AiFillCustomerService } from "react-icons/ai";
+import { BiDownload } from "react-icons/bi";
 
 export default function Home(props) {
   const t = props.t;
@@ -44,26 +46,28 @@ export default function Home(props) {
     <div className="container flex flex-col p-8 items-center gap-8">
       <h1 className="title text-yellow-500 font-bold text-5xl">Speakify</h1>
       <p className="description"></p>
-      <div className="container flex flex-col justify-center items-center min-w-max">
+      <div className="container flex flex-col justify-center items-center min-w-max ">
         <textarea
           className="textinput h-[70px] lg-4 mb-4 w-full lg:w-full m-auto block text-lg"
           id="texto"
           placeholder="Escribir texto"
         ></textarea>
         <div className="button-container grid gap-2">
-          <button
-            className="button w-full active:bg-yellow-500 bg-sky-500 text-black"
-            onClick={handleConvertClick}
-            to="/"
-          >
-            Escuchar
-          </button>
-          <button
-            className="button w-full active:bg-pink bg-pink text-black"
-            onClick={handleDownloadClick}
-          >
-            Descargar
-          </button>
+          
+            <button className="button w-full active:bg-yellow-500 bg-sky-500 text-black flex items-center justify-center">
+  <span className="mr-2">Escuchar</span>
+  <AiFillCustomerService />
+</button>
+
+            
+          
+          
+            <button className="button w-full active:bg-yellow-500 bg-sky-500 text-black flex items-center justify-center">
+  <span className="mr-2">Download</span>
+  <BiDownload />
+</button>
+
+          
         </div>
       </div>
       <div className="text-yellow-500">
